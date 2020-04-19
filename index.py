025b304eca9ceaa535140d5c5558d2642aef0184
khanji3101 <khanji3101@gmail.com>
2020-04-19 23:01:30 +0900

    update index.py

--- index.py
+++ index.py
@@ -1,33 +1,44 @@
+# Flask などの必要なライブラリをインポートする
 from flask import Flask, render_template, request, redirect, url_for
-from bs4   import BeautifulSoup
 import numpy as np
-import pandas as ps
 
+# 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
+# メッセージをランダムに表示するメソッド
 def picked_up():
     messages = [
-        'こんにちは、あなたの名前はなんですか？',
-        'やあ！お名前はなんですか？',
-        'あなたの名前を教えてね？？'
+        "こんにちは、あなたの名前を入力してください",
+        "やあ！お名前は何ですか？",
+        "あなたの名前を教えてね"
     ]
+    # NumPy の random.choice で配列からランダムに取り出し
     return np.random.choice(messages)
 
+# ここからウェブアプリケーション用のルーティングを記述
+# index にアクセスしたときの処理
 @app.route('/')
 def index():
-    title   = 'ようこそ！！'
+    title = "ようこそ"
     message = picked_up()
-    return render_template('index.html', message=message, title=title)
+    # index.html をレンダリングする
+    return render_template('index.html',
+                           message=message, title=title)
 
-@app.route('/post',methods = ['GET','POST'])
+# /post にアクセスしたときの処理
+@app.route('/post', methods=['GET', 'POST'])
 def post():
-    title = 'こんにちは'
+    title = "こんにちは"
     if request.method == 'POST':
+        # リクエストフォームから「名前」を取得して
         name = request.form['name']
-        return render_template('index.html',name=name,title=title)
+        # index.html をレンダリングする
+        return render_template('index.html',
+                               name=name, title=title)
     else:
+        # エラーなどでリダイレクトしたい場合はこんな感じで
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host = '0.0.0.0')
+    app.debug = True # デバッグモード有効化
+    app.run(host='0.0.0.0') # どこからでもアクセス可能に
